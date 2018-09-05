0f385adf8c8a19436ba6f3a02a3d660a554a658b
Bunny-Ingress <kg.prabhani@gmail.com>
2018-09-05 22:27:34 +0530

    eleven commit

---
+++ src/main/java/SeleniumBasics/SearchGoogle.java
@@ -0,0 +1,41 @@
+package SeleniumBasics;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+
+//testng listner(it use to override testng listners)
+@Listeners(SeleniumBasics.ListenerTest.class)    
+public class SearchGoogle {
+    
+    //WebDriver driver;
+    
+
+
+
+@Test
+
+    public void testSearch()
+    {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Prabhani\\Downloads\\Selinium\\chrome\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.lk");
+        
+        
+        driver.findElement(By.id("lst-ib")).sendKeys("Selenium");
+        driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+        
+        String webTitle = driver.getTitle();
+        
+        Assert.assertEquals(webTitle, "Selenium - Web Browser Automation");
+        driver.close();
+        driver.quit();
+    }
+
+
+}
